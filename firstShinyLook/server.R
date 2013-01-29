891eda2737a26c73315d611eb0e0897930a4dec0
erichhuang <erich@mac.com>
2013-01-28 16:34:32 -0800

    Working on server logic.

--- firstShinyLook/server.R
+++ firstShinyLook/server.R
@@ -6,12 +6,19 @@
 require(shiny)
 require(synapseClient)
 
-# Load 3 anonymized patient samples
+## Load 3 anonymized patient samples
 patEnt <- loadEntity('syn1661926')
 
+## Server logic
 shinyServer(function(input, output){
+  
+  ## Patient selection logic
   chooseSample <- reactive(function(){
     whichPatient <- input$variable
     patientSample <- patEnt$objects$patientList[[whichPatient]]
   })
+  
+  ## Model prediction
+  output$prediction
+  
 })
